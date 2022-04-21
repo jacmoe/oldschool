a48255fd1aba1e736890b3675e9acb7751b0b165
Jacob Moena <jacmoe.dk@gmail.com>
2022-04-22 00:26:14 +0200

    Arkscript should not be included in the repository

--- 3rdparty/Ark/docs/tutorials.md
+++
@@ -1,6 +0,0 @@
-@page tutorials Tutorials
-
-1. @subpage brief_architecture
-2. @subpage embedding
-3. @subpage modules
-4. @subpage builtins
--- 3rdparty/Ark/lib/modules/msgpack/include/pack.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ARK_MSGPACK_PACK_HPP
-#define ARK_MSGPACK_PACK_HPP
-
-#include <objects.hpp>
-
-Value msgpack_pack(std::vector<Value>& args, VM* vm);
-
-#endif
